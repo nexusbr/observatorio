78e82e3a3b4bcd0da33667912ef9926f6fbe0cb0
José Maria Villac Pinheiro <jmpinheiro@nexusbr.com>
2015-10-01 17:25:00 -0300

    Colocados scripts para criação do banco de dados, fornecidos pelo David.

---
+++ bancoDados/Geopixel.sql
@@ -0,0 +1,12 @@
+﻿-- Database: "Geopixel"
+
+-- DROP DATABASE "Geopixel";
+
+CREATE DATABASE "Geopixel"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'C'
+       LC_CTYPE = 'C'
+       CONNECTION LIMIT = -1;
+
